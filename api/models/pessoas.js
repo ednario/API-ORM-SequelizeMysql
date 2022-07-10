5538a32f03263116f8695181aadeca2cbe0fce47
Ednario Andrade <joseednario637@gmail.com>
2022-07-10 01:45:39 -0300

    first commit

---
+++ api/models/pessoas.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, DataTypes) => {
+  const Pessoas = sequelize.define(
+    'Pessoas',
+    {
+      nome: DataTypes.STRING,
+      ativo: DataTypes.BOOLEAN,
+      email: DataTypes.STRING,
+      role: DataTypes.STRING,
+    },
+    {},
+  );
+  Pessoas.associate = function (models) {};
+  return Pessoas;
+};
