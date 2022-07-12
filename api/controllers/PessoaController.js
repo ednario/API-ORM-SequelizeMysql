6de9e0008850da7855f1daf7b6e0a95afa83db11
Ednario Andrade <joseednario637@gmail.com>
2022-07-12 00:15:06 -0300

    method de listagem uma única pessoa incrementado

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -1,12 +1,25 @@
 const database = require('../models');
 
 class PessoaController {
+  // listagem de pessoas
   static async pegaTodasAsPessoas(req, res) {
     try {
       const todasAsPessoas = await database.Pessoas.findAll();
       return res.status(200).json(todasAsPessoas);
     } catch (err) {
       return res.status(500).json(err.message);
+    }
+  }
+
+  static async pegaUmaPessoa(req, res) {
+    const { id } = req.params;
+    try {
+      const umaPessoa = await database.Pessoas.findOne({
+        where: { id: Number(id) },
+      });
+      return res.status(200).json(umaPessoa);
+    } catch (error) {
+      return res.status(500).send(error);
     }
   }
 }
