10b17d0683814193fac31658bd2c5fd5aa809693
Ednario Andrade <joseednario637@gmail.com>
2022-07-12 00:58:51 -0300

    method para deleta uma pessoa incrementado

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -49,6 +49,19 @@ class PessoaController {
       return res.status(500).send(error.message);
     }
   }
+
+  // deletar uma pessoa
+  static async apagaPessoa(req, res) {
+    const { id } = req.params;
+    try {
+      await database.Pessoas.destroy({
+        where: { id: Number(id) },
+      });
+      return res.status(200).send(`Pessoa referente ao id ${id} deletado.`);
+    } catch (error) {
+      return res.status(500).send(error.message);
+    }
+  }
 }
 
 module.exports = PessoaController;
