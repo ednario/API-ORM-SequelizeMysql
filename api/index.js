941a80cd2ea97cb710ae2b1201bf27bf8c66fd50
Ednario Andrade <joseednario637@gmail.com>
2022-07-11 22:43:24 -0300

    Rotas organizadas

--- api/index.js
+++ api/index.js
@@ -1,16 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const routes = require('./routes');
 
 const app = express();
-
-app.use(bodyParser.json());
-
 const port = 3000;
 
-app.get('/teste', (req, res) =>
-  res.status(200).send({ message: 'bem vindo a API' }),
-);
+routes(app);
 
-app.listen(port, () => console.log(`Server is runner ${port}`));
+app.listen(port, () => console.log(`Server is runner on port ${port}`));
 
 module.exports = app;
--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,6 @@
 module.exports = {
+  semi: true,
   singleQuote: true,
-  trailingComma: "all",
-  arrowParens: "avoid",
+  tabWidth: 2,
+  useTabs: false
 };
