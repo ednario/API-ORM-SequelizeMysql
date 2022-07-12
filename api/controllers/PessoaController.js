db03b1d0d6482578ca16d3cf632a5800d95f074f
Ednario Andrade <joseednario637@gmail.com>
2022-07-11 23:37:01 -0300

    rota de listagem de pessoas incrementada.

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -2,12 +2,11 @@ const database = require('../models');
 
 class PessoaController {
   static async pegaTodasAsPessoas(req, res) {
-    try{
-      const todasAsPessoas = await database.Pessoa.findAll();
+    try {
+      const todasAsPessoas = await database.Pessoas.findAll();
       return res.status(200).json(todasAsPessoas);
-    } catch(err) {
-      return res.status(500).json(err.message
-      );
+    } catch (err) {
+      return res.status(500).json(err.message);
     }
   }
 }
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,6 +1,8 @@
 const bodyParser = require('body-parser');
+const pessoas = require('./pessoasRoute');
 
 module.exports = (app) => {
   app.use(bodyParser.json());
-  app.get('/', (req, res) => res.status(200).send("Olá, mundo!"));
-}
+  app.use(pessoas);
+  app.get('/', (req, res) => res.status(200).send('Olá, mundo!'));
+};
